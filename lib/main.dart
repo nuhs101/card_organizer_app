4bb629d8526e21c0176112f8edd1b2704e73b47d
Brian <aladebrian@gmail.com>
2025-03-05 16:03:00 -0500

    Added temporary assets, created text-based deck

--- lib/main.dart
+++ lib/main.dart
@@ -18,36 +18,85 @@ class CardOrganizerApp extends StatelessWidget {
 }
 
 class FolderScreen extends StatelessWidget {
-  final List<String> folders = ['Heart', 'Spades', 'Diamonds', 'Clubs'];
-
+  final List<String> folders = ['Hearts', 'Spades', 'Diamonds', 'Clubs'];
+  final List<String> suits = ["hearts", 'spades', 'diamonds', 'clubs'];
+  final List<String> numbers = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11',
+    '12',
+    '13',
+  ];
   FolderScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Card Organizer')),
-      body: ListView.builder(
-        itemCount: folders.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              title: Text(folders[index]),
-              trailing: IconButton(
-                icon: Icon(Icons.arrow_forward),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder:
-                          (context) => CardScreen(folderName: folders[index]),
+      body: Column(
+        children: [
+          //Folders
+          Expanded(
+            child: ListView.builder(
+              itemCount: folders.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title: Text("${folders[index]} ${numbers[0]}"),
+                    trailing: IconButton(
+                      icon: Icon(Icons.arrow_forward),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder:
+                                (context) =>
+                                    CardScreen(folderName: folders[index]),
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             ),
-          );
-        },
+          ),
+          // Deck
+          Expanded(
+            child: ListView.builder(
+              itemCount: 4,
+              itemBuilder: (context, suitIndex) {
+                return GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 5,
+                  ),
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: numbers.length,
+                  itemBuilder: (context, numberIndex) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GridTile(
+                        child: Text(
+                          "${suits[suitIndex]}${numbers[numberIndex]}",
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
